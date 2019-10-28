b89b39233bb23a73efb7be32b097b73b1a4f591d
Nhan Cao <nhancv92@gmail.com>
2019-10-28 12:14:56 +0700

    update android launcher

--- example/android/app/src/main/kotlin/com/example/bflutter_poc/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/example/bflutter_poc/MainActivity.kt
@@ -1,13 +1,29 @@
 package com.example.bflutter_poc
 
+import android.os.Build
 import android.os.Bundle
+import android.view.ViewTreeObserver
+import android.view.WindowManager
 
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    //make transparent status bar
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+      window.statusBarColor = 0x00000000
+    }
     GeneratedPluginRegistrant.registerWith(this)
+    //Remove full screen flag after load
+    val vto: ViewTreeObserver = flutterView.viewTreeObserver
+    vto.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+      override fun onGlobalLayout() {
+        // your code here. `this` should work
+        flutterView.viewTreeObserver.removeOnGlobalLayoutListener(this)
+        window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+      }
+    })
   }
 }
