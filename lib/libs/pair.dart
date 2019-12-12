9f1bdcdfc2af0106d8f463733095989b282ad695
Nhan Cao <nhancv92@gmail.com>
2019-12-12 11:18:08 +0700

    add pair

---
+++ lib/libs/pair.dart
@@ -0,0 +1,15 @@
+/*
+ * Developed by Nhan Cao on 12/12/19 11:17 AM.
+ * Last modified 12/12/19 11:17 AM.
+ * Copyright (c) 2019 Beesight Soft. All rights reserved.
+ */
+
+class Pair {
+  Pair(this.left, this.right);
+
+  final dynamic left;
+  final dynamic right;
+
+  @override
+  String toString() => 'Pair[$left, $right]';
+}
